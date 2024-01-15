718e5427f07b0d816c38d07ac7c8c8b8dbeb647b
Arterning <ning.huang.ridson@gmail.com>
2024-01-15 16:02:23 +0800

    Update

--- src/scripts/seedRole.ts
+++ src/scripts/seedRole.ts
@@ -1,4 +1,10 @@
+import mongoose from 'mongoose';
 import { createRole, getRoles } from "../db/roles";
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost:27017/rest-api');
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
 
 (async () => {
     console.log("Seeding roles");
@@ -6,4 +12,6 @@ import { createRole, getRoles } from "../db/roles";
     await createRole({ name: "user" , permissions: ["users.read", "users.write"]});
     console.log("Seeded roles");
     await getRoles().then(roles => console.log(roles));
+    //why we should exit here
+    process.exit(0);
 })();
